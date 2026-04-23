2d54da9be7ea6d89d912456b66d440c3bd5ed094
Emiliano Juan <emiliano.juan.dev@gmail.com>
2026-04-23 20:03:54 -0300

    mejora busqueda ML

--- app.py
+++ app.py
@@ -47,11 +47,18 @@ def clean_price(price_str):
 
 def get_search_results(query):
     headers = {
-        'User-Agent': 'Mozilla/5.0',
-        'Accept-Language': 'es-ES,es;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'es-AR,es;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
     }
     url = f"https://listado.mercadolibre.com.ar/{query}"
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, timeout=10)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     product_links = []
